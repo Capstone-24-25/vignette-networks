7333bdcb2e5c8628f44839875714da1656d58288
ili772 <ili@ucsb.edu>
2024-12-02 20:20:38 -0800

    update bootstrap files

--- scripts/bootstrap-test.R
+++ scripts/bootstrap-test.R
@@ -14,7 +14,7 @@ bootstrap.t.test <- function(graph, subgraph, statistic, n) {
   # extract function to get statistic
   statistic <- get(statistic, mode = "function")
   
-  # caculate distribution of the statistic of interest through random sampling
+  # calculate distribution of the statistic of interest through random sampling
   stat.dist <- lapply(
     1:n,
     function(i) {
@@ -48,3 +48,109 @@ bootstrap.t.test <- function(graph, subgraph, statistic, n) {
   
   return(out)
 }
+
+source("scripts/preprocessing.R")
+
+cnet.igraph <- fromJSON(file = "data/congress_network/congress_network_data.json") %>%
+  cnet_json_to_df() %>%
+  create_cnet_igraph()
+
+
+
+cnet.subgraph.dem <- cnet.igraph %>%
+  subgraph(
+    cnet.igraph %>%
+      vertex_attr(
+        "Party"
+      ) %>%
+      `==`("D") 
+  )
+
+cnet.subgraph.rep <- cnet.igraph %>%
+  subgraph(
+    cnet.igraph %>%
+      vertex_attr(
+        "Party"
+      ) %>%
+      `==`("R") 
+  )
+
+cnet.subgraph.senate <- cnet.igraph %>%
+  subgraph(
+    cnet.igraph %>%
+      vertex_attr(
+        "Chamber"
+      ) %>%
+      `==`("Senate")
+  )
+
+cnet.subgraph.house <- cnet.igraph %>%
+  subgraph(
+    cnet.igraph %>%
+      vertex_attr(
+        "Chamber"
+      ) %>%
+      `==`("House")
+  )
+
+cnet.subgraph.midwest <- cnet.igraph %>%
+  subgraph(
+    cnet.igraph %>%
+      vertex_attr(
+        "Region"
+      ) %>%
+      `==`("Midwest") %>%
+      replace_na(FALSE)
+    
+  )
+
+cnet.subgraph.west <- cnet.igraph %>%
+  subgraph(
+    cnet.igraph %>%
+      vertex_attr(
+        "Region"
+      ) %>%
+      `==`("West") %>%
+      replace_na(FALSE)
+  )
+
+cnet.subgraph.south <- cnet.igraph %>%
+  subgraph(
+    cnet.igraph %>%
+      vertex_attr(
+        "Region"
+      ) %>%
+      `==`("South") %>%
+      replace_na(FALSE)
+  )
+
+cnet.subgraph.northeast <- cnet.igraph %>%
+  subgraph(
+    cnet.igraph %>%
+      vertex_attr(
+        "Region"
+      ) %>%
+      `==`("Northeast") %>%
+      replace_na(FALSE)
+  )
+
+graphs <- paste(rep("cnet.subgraph", 8), 
+                c("dem", "rep", "house", "senate", "midwest", "west", "south", "northeast"),
+                sep = ".")
+
+metrics <- c("reciprocity", "transitivity", "edge_density", "mean_distance")
+
+bootstrap.test.results <- matrix(
+  data = rep(rep(0, length(groups)), length(metrics)),
+  nrow = length(graphs), 
+  ncol = length(metrics), 
+  dimnames = (list(c("dem", "rep", "house", "senate", "midwest", "west", "south", "northeast"), metrics))
+)
+
+for (i in 1:length(graphs)) {
+  for (j in 1:length(metrics)) {
+    bootstrap.test.results[i,j] <- bootstrap.t.test(cnet.igraph, get(graphs[i]), metrics[j], 10000)$pval
+  }
+}
+
+save(bootstrap.test.results, "data/bootstrap-test-results.RData")
